72f1eb47f5e65c1421067d76a32efb9ffe852d8e
Ludaro <waschmaschvanlu@gmail.com>
2023-09-01 13:35:15 +0200

    Create changelog.lua

---
+++ server/changelog.lua
@@ -0,0 +1,9 @@
+
+changelog = {
+    alpha01 = "Initial Release",
+    alpha02 = "Added Job Menu",
+    alpha03 = "Added Society Menu",
+    alpha04 = "Added Interactions",
+    alpha05 = "added Version-Checker",
+    alpha06 = "Added Boss-Menu",
+}
