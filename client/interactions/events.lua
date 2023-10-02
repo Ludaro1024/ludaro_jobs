fc644f1b4d376d255adca35bd107ba3ef912e7db
Mirow <maxim.doerr@icloud.com>
2023-10-03 00:18:57 +0200

    🎨 removed random spaces

--- client/interactions/events.lua
+++ client/interactions/events.lua
@@ -1,6 +1,5 @@
-
 RegisterNetEvent('ludaro_jobs:interactionserror')
 AddEventHandler('ludaro_jobs:interactionserror', function()
-debug2("error while starting event!", 1)
-Config.Notify("error while using Interaction!")
+    debug2("error while starting event!", 1)
+    Config.Notify("error while using Interaction!")
 end)
